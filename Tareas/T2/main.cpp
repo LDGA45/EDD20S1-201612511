affe8534655697db5f88979de4e9ca0c93c764e7
LDGA45 <36612290+LDGA45@users.noreply.github.com>
2020-02-01 23:57:49 -0600

    Create main.cpp

---
+++ Tareas/T2/main.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <iostream>
+#include "moneda.h"
+
+using namespace std;
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+
+	
+
+	int m[2][5]={{0,1,2,3,4},{60,10,10,10,10}};
+	
+    moneda m1 = moneda();
+
+
+
+	system("pause");
+	return 0;
+}
