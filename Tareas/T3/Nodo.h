f6563c3c0a3075c2367ade3ba5d320109b335cad
LDGA45 <36612290+LDGA45@users.noreply.github.com>
2020-02-11 21:30:27 -0600

    Update Nodo.h

--- Tareas/T3/Nodo.h
+++ Tareas/T3/Nodo.h
@@ -1,15 +1,16 @@
+
 #include <iostream>
-#include <cstdlib>
 #include <string>
 
 using namespace std;
 
-class Nodo{
+class Nodo
+{
 public:
 	Nodo(int id , string nombre);
 	string nombre;
 	int id;
 	Nodo* siguiente;
-	
+
 
 };
