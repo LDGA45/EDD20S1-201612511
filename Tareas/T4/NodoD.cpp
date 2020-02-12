383b089a3256cd52eca9114c7abcc6e6ab3d73d8
LDGA45 <36612290+LDGA45@users.noreply.github.com>
2020-02-11 22:18:43 -0600

    Update NodoD.cpp

--- Tareas/T4/NodoD.cpp
+++ Tareas/T4/NodoD.cpp
@@ -1,11 +1,10 @@
 #include "NodoD.h"
-#include <iostream>
-#include <string>
-using namespace std;
 
-NodoD::NodoD(char _letra){
-    letra = _letra;
-    siguiente = NULL;
-    anterior = NULL;
+
+
+NodoDoble::NodoDoble(char _letra)
+{
+	letra = _letra;
+	siguiente = NULL;
+	anterior = NULL;
 }
-
