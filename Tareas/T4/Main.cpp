038413baeb78af25f2f2049694d87ea883a76647
LDGA45 <36612290+LDGA45@users.noreply.github.com>
2020-02-11 22:18:40 -0600

    Update Main.cpp

--- Tareas/T4/Main.cpp
+++ Tareas/T4/Main.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include "ListaD.h"
 using namespace std;
-
-#include "Lista.h"
 
 int main(int argc, char *argv[])
 {
-    
-	Lista lista = Lista();
-    
-    lista.insertarPrimero('p');
-    lista.insertarPrimero('O');
-    lista.insertarPrimero('o');
-    lista.insertarPrimero('O');
-    lista.show();
+    ListaDoble lista = ListaDoble();
 
+	string a = "hola";
+
+	lista.AgregarPrimero('N');
+	lista.AgregarPrimero('P');
+	lista.AgregarPrimero('O');
+	lista.AgregarPrimero('C');
+	lista.AgregarPrimero('D');
+	
+	lista.AgregarPrimero('R');
+	lista.AgregarPrimero('E');
+	lista.AgregarPrimero('E');
+	lista.AgregarPrimero('L');
+
+	lista.show();
+
+	lista.EliminarULTIMO();
+	cout << "\n";
+	lista.show();
+	lista.EliminarULTIMO();
+	cout << "\n";
+	lista.show();
+	cout << "\n";
+
+	cout << lista.busqueda("LEER")->letra;
+
+	
 	system("pause");
+	return 0;
 }
